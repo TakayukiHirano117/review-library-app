58c3428b91f90ddcdeaac629f29a965057d49d59
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-05-05 19:03:03 +0900

    app.tsの修正

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from "express";
+import { BookController } from "./presentation/bookController";
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const bookController = new BookController();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello" });
-});
+app.post("/books", bookController.add.bind(bookController));
+app.get("/books/:id", bookController.findById.bind(bookController));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
